da1bd4101f93958b0071eec8c23344a1bdf9d3ed
Marina <marinka18_09@ukr.net>
2017-04-07 00:28:36 +0300

    Them underscores + gulp

---
+++ wp-content/themes/exam/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * exam Theme Customizer
+ *
+ * @package exam
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function exam_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'exam_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function exam_customize_preview_js() {
+	wp_enqueue_script( 'exam_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'exam_customize_preview_js' );
